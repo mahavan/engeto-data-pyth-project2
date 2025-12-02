b30075a9b4d94f913be80e3caec36db26eb71ad1
mahavan <info@mahovsky.net>
2025-12-03 00:18:56 +0100

    add secret_number function

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Project 2: Game Bulls & Cows
+"""
+import random
+
+def secret_number() -> str:
+    """
+    Generate a unique number as a string of NUMBER_LENGTH digits, 
+    where the first digit is not zero.
+    """
+    # Length of the secret number must be <= 10
+    NUMBER_LENGTH = 4 
+    digits = [i for i in range(10)]
+    random.shuffle(digits)
+    while digits[0] == 0:
+        random.shuffle(digits) 
+    # Select the first NUMBER_LENGTH digits and join them into one number.
+    return ''.join(map(str, digits[:NUMBER_LENGTH])) 
+
+if __name__ == "__main__":
+
+    print("Secret number for debugging:", secret_number())
